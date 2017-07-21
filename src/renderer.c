00031fcf1817acd80bade61427e51945e849198d
Nikki Koole <nikki@floorplanner.com>
2017-07-22 00:37:13 +0100

    officedome

--- src/renderer.c
+++ src/renderer.c
@@ -349,7 +349,7 @@ void prepare_renderer(PermanentState *permanent, RenderState *renderer) {
                 float tempY = (data.z + data.frame.y_off + data.frame.y_internal_off) - (data.y) / 2;
                 if (data.is_floor) {
                     // TODO this offset is still bugging me
-                    wallDepth = data.y - 20;
+                    wallDepth = data.y  - 20;
                     //pivotY = (108.0f) /(108.0f - 12.0f);
                     //tempY -= 12;
                 }
